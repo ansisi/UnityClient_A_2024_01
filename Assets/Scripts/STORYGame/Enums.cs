c4dd6cc203dea36a30439aa5375db106de01d3dc
ansisi <Sihyeona73@gmail.com>
2024-03-26 11:26:31 +0900

    [feat]스크립터블 오브젝트생성
    헤헷

--- Assets/Scripts/STORYGame/Enums.cs
+++ Assets/Scripts/STORYGame/Enums.cs
@@ -26,5 +26,38 @@ namespace STORYGAME
             CheckCHA,
         }
 
+        public enum ResultType
+        {
+
+            ChangeHp,
+            ChangeSp,
+            AddExlerience,
+            GoToShop,
+            GoToNextStroy,
+            GoToRandomStory,
+            GoToEnding
+        }
+
     }
+   
+
+    [System.Serializable]
+
+    public class Stats
+    {
+        public int hpPoint;
+        public int spPoint;
+
+        public int currentHpPoint;
+        public int currentSpPoint;
+        public int currentXpPoint;
+
+        public int strenght;
+        public int dexterity;
+        public int consitiution;
+        public int intelligence;
+        public int wisdom;
+        public int sharisma;
+    }
+
 }
